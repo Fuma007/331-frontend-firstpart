6984cc6f7cbb1bf0607740239aec139b16cf10bc
Fuma007 <techit_wongkiti@cmu.ac.th>
2026-09-21 14:46:59 +0700

    lab08-6.5 commit

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,6 +10,7 @@ import NotFoundView from '@/views/NotFoundView.vue'
 import NetworkErrorView from '@/views/NetworkErrorView.vue'
 import nProgress from 'nprogress'
 import { useEventStore } from '@/stores/event'
+import AddEventView from '@/views/event/EventFormView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -23,6 +24,11 @@ const router = createRouter({
       path: '/about',
       name: 'about',
       component: AboutView,
+    },
+    {
+      path: '/add-event',
+      name: 'add-event',
+      component: AddEventView,
     },
     {
       path: '/event/:id',
